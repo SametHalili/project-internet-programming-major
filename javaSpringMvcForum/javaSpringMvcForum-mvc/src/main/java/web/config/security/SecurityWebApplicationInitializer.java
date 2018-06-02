6166c25bdea5f7d8d75d4ed6af63dad0d243e138
SametHalili <sametalenvers@gmail.com>
2018-06-02 17:01:17 +0200

    Wiping to fix project not working correctly on server

--- javaSpringMvcForum/javaSpringMvcForum-mvc/src/main/java/web/config/security/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package web.config.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer
-{
-
-}
